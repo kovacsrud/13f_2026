7d6a891785aba7e163814d6179a9dadbf39c8c26
kovacsrud <kovacs.rud@gmail.com>
2026-01-26 14:00:19 +0100

    Aritmetikai műveletek

---
+++ Valtozok/AritmetikaiMuveletek/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace AritmetikaiMuveletek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aritmetikai műveletek");
+
+            int a = 127;
+            int b = 31;
+
+            //Növelés/csökkentés
+
+            a++;
+
+            ++a;
+
+            Console.WriteLine(a);
+
+            //csökkentés
+            a--;
+            --a;
+
+            Console.WriteLine(a);
+
+            //Trükkös!
+            int c = a++;
+            //int c = ++a;
+
+            Console.WriteLine(c);
+
+            //Aritmetikai műveletek : +,-,*,/,%
+            c = c + a;
+            c += a;
+            c -= a;
+            c *= a;
+            c /= a;
+
+            //Egész osztás
+            int d = a / b;
+
+            Console.WriteLine($"D:{d}");
+
+            //Maradékos osztás
+            d = a % 3;
+
+            Console.WriteLine($"D:{d}");
+
+            //Osztás a maradékkal, típus kényszerítéssel
+            double maradekos = (double)a / b;
+
+            Console.WriteLine($"Maradékos:{maradekos}");
+
+        }
+    }
+}
