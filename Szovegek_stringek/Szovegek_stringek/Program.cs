4f09608324ee92d6fa273c5012d0cc07df8effc9
kovacsrud <kovacs.rud@gmail.com>
2026-01-27 14:13:16 +0100

    Update Program.cs

--- Szovegek_stringek/Szovegek_stringek/Program.cs
+++ Szovegek_stringek/Szovegek_stringek/Program.cs
@@ -46,6 +46,37 @@
                 Console.WriteLine("Megegyeznek a szövegek");
             }
 
+            //Karakterek cseréje
+            Console.WriteLine(szoveg.Replace('e','x'));
+
+            //szöveg cseréje
+            Console.WriteLine(szoveg.Replace("típus","változó"));
+
+            //Szöveg első/utolsó karaktere
+            //Indexeléssel
+            Console.WriteLine(szoveg[0]);
+            Console.WriteLine(szoveg[szoveg.Length-1]);
+
+            //Függvénnyel
+            Console.WriteLine(szoveg.First());
+            Console.WriteLine(szoveg.Last());
+
+            //Rész szöveg kiemelése
+
+            var datum = "2009.12.26";
+
+            //var -> a bal oldali változó a jobb oldali értéknek megfelelő típust fogja felvenni
+            var ev = datum.Substring(0,4);
+            var honap=datum.Substring(5,2);
+            var nap = datum.Substring(8,2);
+
+            Console.WriteLine($"Ev:{ev},hónap:{honap},nap:{nap}");
+
+            //string darabolása egy karakter mentén
+            var datumElemek = datum.Split('.');
+            Console.WriteLine($"Év:{datumElemek[0]},hónap:{datumElemek[1]},nap:{datumElemek[2]}");
+
+
 
         }
     }
