fc8035edf26fdd74e475fbe3ca045da5273f3b12
kovacsrud <kovacs.rud@gmail.com>
2026-03-20 10:36:41 +0100

    Kutya adatok betöltése

---
+++ Kutyak/Kutyak/Betolto.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kutyak
+{
+    public static class Betolto
+    {
+        public static List<Kutyanev> BetoltKutyanev(string fajl,char hatarolo,int start=1)
+        {
+            List<Kutyanev> kutyanevek=new List<Kutyanev>();
+
+            var adatok = File.ReadAllLines(fajl,Encoding.Default);
+
+            for (int i = start; i < adatok.Length; i++)
+            {
+                kutyanevek.Add(new Kutyanev(adatok[i],hatarolo));
+            }
+
+            return kutyanevek;
+        }
+
+        public static List<Kutyafajta> BetoltKutyafajta(string fajl, char hatarolo, int start = 1)
+        {
+            List<Kutyafajta> kutyafajtak=new List<Kutyafajta>();
+
+            var adatok = File.ReadAllLines(fajl, Encoding.Default);
+
+            for (int i = start; i < adatok.Length; i++)
+            {
+                kutyafajtak.Add(new Kutyafajta(adatok[i], hatarolo));
+            }
+
+            return kutyafajtak;
+        }
+
+        public static List<Kutya> BetoltKutya(string fajl, char hatarolo, int start = 1)
+        {
+            List<Kutya> kutyak=new List<Kutya>();
+
+            var adatok = File.ReadAllLines(fajl, Encoding.Default);
+
+            for (int i = start; i < adatok.Length; i++)
+            {
+                kutyak.Add(new Kutya(adatok[i], hatarolo));
+            }
+
+
+            return kutyak;
+        }
+    }
+}
