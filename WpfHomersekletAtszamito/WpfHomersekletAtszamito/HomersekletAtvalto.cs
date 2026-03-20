28213e4872fcf3aa9facb637449309ee17cac52c
kovacsrud <kovacs.rud@gmail.com>
2026-03-20 09:01:35 +0100

    Hőmérséklet konverter

---
+++ WpfHomersekletAtszamito/WpfHomersekletAtszamito/HomersekletAtvalto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfHomersekletAtszamito
+{
+    //A statikus osztálynak csak egy példánya van és nem kell példányosítani
+    public static class HomersekletAtvalto
+    {
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 1.8) + 32;
+        }
+
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            return (fahrenheit - 32) / 1.8;
+        }
+    }
+}
---
+++ WpfHomersekletAtszamito/WpfHomersekletAtszamito/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfHomersekletAtszamito
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            //Grafikus felület inicializálása
+            //Ha "véletlenül" kitöröljük, akkor a program futásakor egy üres ablakot fogunk látni
+            InitializeComponent();
+            this.Title = "Hőmérséklet átszámítás";
+        }
+
+        private void buttonSzamol_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (radioCelsius.IsChecked==true)
+                {
+                    textblockEredmeny.Text = HomersekletAtvalto.FahrenheitToCelsius(Convert.ToDouble(textboxHomerseklet.Text)).ToString()+" C";
+                } else
+                {
+                    textblockEredmeny.Text = HomersekletAtvalto.CelsiusToFahrenheit(Convert.ToDouble(textboxHomerseklet.Text)).ToString()+" F";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Hiba!");                
+            }
+        }
+    }
+}
