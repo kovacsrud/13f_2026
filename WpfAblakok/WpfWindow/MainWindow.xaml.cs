a173b5d76732a13a5edf42cebedebf9bea09db8e
kovacsrud <kovacs.rud@gmail.com>
2026-03-23 13:35:29 +0100

    Több ablakos program

---
+++ WpfAblakok/WpfWindow/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfWindow.views;
+
+namespace WpfWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void menuitemKilepes_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void menitemAblak1_Click(object sender, RoutedEventArgs e)
+        {
+            Ablak1 ablak1 = new Ablak1();
+            ablak1.Show();
+        }
+
+        private void menitemAblak2_Click(object sender, RoutedEventArgs e)
+        {
+            Ablak2 ablak2 = new Ablak2();
+            ablak2.ShowDialog();
+        }
+
+        private void menitemAblak3_Click(object sender, RoutedEventArgs e)
+        {
+            Ablak3 ablak3 = new Ablak3();
+            ablak3.ShowDialog();
+        }
+    }
+}
