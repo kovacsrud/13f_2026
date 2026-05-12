04906ad4eed9c3eda0350b72597417cb226dbe61
kovacsrud <kovacs.rud@gmail.com>
2026-05-12 09:44:46 +0200

    Wpf időzítő

---
+++ Idozitok/Idozitok/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Timers;
+
+namespace Idozitok
+{
+    internal class Program
+    {
+        static int szamlalo = 0;
+        static void Main(string[] args)
+        {
+            System.Timers.Timer timer = new System.Timers.Timer(1000);
+            timer.Elapsed += Callback;
+            timer.Start();
+            Console.ReadKey();
+        }
+
+        private static void Callback(object sender, ElapsedEventArgs e){
+            Console.WriteLine($"{szamlalo}-{DateTime.Now.Second}");
+            szamlalo++;
+        }
+    }
+}
