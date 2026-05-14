ef5095d4bb51c373fb303d5d7b29f2867df4f0ea
kovacsrud <kovacs.rud@gmail.com>
2026-05-14 10:36:16 +0200

    Adapter osztály készítése

---
+++ WpfAdatBinding/WpfAdatBinding/fileload/LoadFromFile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfAdatBinding.model;
+
+namespace WpfAdatBinding.fileload
+{
+    public static class LoadFromFile
+    {
+        public static List<Airport> LoadAirports(string fajl,char hatarolo,int start=1) {
+            List<Airport> airports = new List<Airport>();
+
+            var sorok=File.ReadAllLines(fajl,Encoding.UTF8);
+
+            for (int i = start; i < sorok.Length; i++)
+            {
+                airports.Add(new Airport(sorok[i], hatarolo));
+            }
+
+
+            return airports;
+        }
+    }
+}
---
+++ WpfAdatBinding/WpfAdatBinding/model/DataSource.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfAdatBinding.fileload;
+
+namespace WpfAdatBinding.model
+{
+    public class DataSource
+    {
+        public List<Airport> Airports { get; set; } = new List<Airport>();
+
+        public DataSource()
+        {
+            try
+            {
+                Airports = LoadFromFile.LoadAirports("airports_pv.csv", ';');
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);                
+            }
+        }
+    }
+}
