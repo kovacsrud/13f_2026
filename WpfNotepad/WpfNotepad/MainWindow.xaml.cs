96366a60b1c20987527052d39d18d9389a6a3519
kovacsrud <kovacs.rud@gmail.com>
2026-05-15 08:40:56 +0200

    Notepad készítés

---
+++ WpfNotepad/WpfNotepad/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfNotepad
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void menuitemMegnyitas_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void menuitemMentes_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void menuitemMentesMaskent_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void menuitemKilepes_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void menuitemKivagas_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void menuitemMasolas_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void menuitemBeillesztes_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
