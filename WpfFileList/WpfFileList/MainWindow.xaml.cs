43a1e49d7007d164a6df57d0eea72da2c7596719
kovacsrud <kovacs.rud@gmail.com>
2026-05-18 13:22:23 +0200

    Lista objektum használata

--- WpfFileList/WpfFileList/MainWindow.xaml.cs
+++ WpfFileList/WpfFileList/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfFileList.model;
 
 namespace WpfFileList
 {
@@ -18,9 +19,14 @@ namespace WpfFileList
     /// </summary>
     public partial class MainWindow : Window
     {
+        public FileList FileList { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            FileList = new FileList();
+
+
+            listboxFajlok.DataContext=FileList;
         }
 
         private void buttonTallozas_Click(object sender, RoutedEventArgs e)
@@ -30,15 +36,24 @@ namespace WpfFileList
             dialog.Multiselect = true;
             if (dialog.ShowDialog()==true)
             {
-                listboxFajlok.ItemsSource = dialog.FileNames;
-                imageKep.Source = new BitmapImage(new Uri(dialog.FileNames.First()));
+                //listboxFajlok.ItemsSource = dialog.FileNames;
+                //imageKep.Source = new BitmapImage(new Uri(dialog.FileNames.First()));
+                FileList.SetFileList(dialog.FileNames,'\\');
+                listboxFajlok.SelectedIndex = 0;
 
             }
         }
 
         private void listboxFajlok_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            imageKep.Source = new BitmapImage(new Uri(listboxFajlok.SelectedItem.ToString()));
+            //imageKep.Source = new BitmapImage(new Uri(listboxFajlok.SelectedItem.ToString()));
+            if (listboxFajlok.SelectedItem!=null)
+            {
+                var selected = (FileItem)listboxFajlok.SelectedItem;
+                imageKep.Source = new BitmapImage(new Uri(selected.FullPath));
+            }
+
+
         }
     }
 }
